8ef3c93fb2e47a1a233ce524be4bb9b41a932012
한철 <chul.han@biplatform.co.kr>
2024-09-22 13:32:02 +0900

    컴밋앤푸시

--- src/main/java/com/example/demo/controller/FindRackGameController.java
+++ src/main/java/com/example/demo/controller/FindRackGameController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.service.FindRackService;
 import com.example.demo.service.FindFlagService;
 import com.example.demo.service.FindSnakeService;
+import com.example.demo.service.FindBinggoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,8 @@ public class FindRackGameController {
 	 private FindFlagService findFlagService;
 	 @Autowired
 	 private FindSnakeService findSnakeService;
+	 @Autowired
+	 private FindBinggoService findBinggoService;
 	 
 	 @PostMapping("/findRockGame")
 	 public Map<String, Object> findRockGame(@RequestBody Map<String, Object> numbers) throws Exception {
@@ -35,6 +38,12 @@ public class FindRackGameController {
 	 @PostMapping("/findSnakeGame")
 	 public Map<String, Integer> findSnakeGame(@RequestBody Map<String, Integer> numbers) throws Exception {
 		 Map<String, Integer> result = findSnakeService.findSnakeGame(numbers);
+	 return result;
+	 }
+	 
+	 @PostMapping("/findBinggoGame")
+	 public Map<String, Integer> findBinggoGame(@RequestBody Map<String, Integer> numbers) throws Exception {
+		 Map<String, Integer> result = findBinggoService.findBinggoGame(numbers);
 	 return result;
 	 }
  
