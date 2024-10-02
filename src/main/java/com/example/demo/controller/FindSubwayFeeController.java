551047d24dfa510db361b703d723291476a30403
Chul Han <hch728@nate.com>
2024-10-03 01:57:35 +0900

    커밋합니다.

---
+++ src/main/java/com/example/demo/controller/FindSubwayFeeController.java
@@ -0,0 +1,32 @@
+
+package com.example.demo.controller;
+
+import com.example.demo.service.CalculationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class FindSubwayFeeController {
+ @Autowired
+ private CalculationService calculationService;
+
+    @PostMapping("/findSumNo")
+    public Map<String, Integer> findSumNo(@RequestBody Map<String, Integer> numbers) throws Exception {
+        Map<String, Integer> result = calculationService.findSumNo(numbers);
+        return result;
+    }
+
+    @PostMapping("/findOddNo")
+        public Map<String, Object> findOddNo(@RequestBody Map<String, Integer> numbers) throws Exception {
+        return calculationService.findOddNo(numbers);
+    }
+
+}
+
+
---
+++ src/main/java/com/example/demo/service/FindSubwayLineService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class FindSubwayLineService {
+
+ public Map<String, Integer> findFlagGame(Map<String, Integer> numberMap) throws Exception {
+		int[] num = new int[10];
+		int totJump=0, cntJump=0;
+		System.out.println(numberMap);
+		num[0] = numberMap.get("num1");
+		num[1] = numberMap.get("num2");
+		num[2] = numberMap.get("num3");
+		num[3] = numberMap.get("num4");
+		num[4] = numberMap.get("num5");
+		num[5] = numberMap.get("num6");
+		num[6] = numberMap.get("num7");
+		num[7] = numberMap.get("num8");
+		num[8] = numberMap.get("num9");
+		num[9] = numberMap.get("num10");
+		
+		while(totJump<10) {
+			totJump+= num[cntJump];
+			cntJump ++; 
+		}
+	    
+	    numberMap.put("totJump", totJump);
+	    numberMap.put("cntJump", cntJump);
+	    return numberMap;
+ }
+ 
+}
